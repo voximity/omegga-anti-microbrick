71faf69a222c17a7c987648decd90274523dc063
Zander <zander@zanderf.net>
2021-10-08 12:19:47 -0500

    feat: improved clearing, violations + punishment

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
-use std::{collections::HashSet, fs::File, path::PathBuf};
+use std::{
+    collections::HashSet,
+    fs::{File, OpenOptions},
+    path::PathBuf,
+    time::Duration,
+};
 
 use anyhow::Result;
-use brickadia::read::SaveReader;
+use brickadia::{read::SaveReader, save::SaveData, write::SaveWriter};
 use chrono::Utc;
 use omegga::{rpc, Omegga};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 pub const ASEZ: &'static str = "autosave_ez";
+pub const SAVES_LOC: &'static str = "../../data/Saved/Builds";
+pub const SAVE_LOC: &'static str = "_anti_microbrick.brs";
 
 #[derive(Serialize, Deserialize)]
 struct Config {
     #[serde(rename = "clear-after-minutes")]
-    clear_after: u32,
+    clear_after: f32,
+
+    #[serde(rename = "max-violations")]
+    max_violations: u32,
+
+    #[serde(rename = "ban-time")]
+    ban_time: f32,
+
+    #[serde(rename = "max-bans")]
+    max_bans: u32,
 }
 
 #[tokio::main]
@@ -83,12 +99,12 @@ async fn check_save(omegga: &Omegga, config: &Config, path: PathBuf) -> Result<(
 
     // at this point, we know we have microbricks, so let's scan the save for them
     reader.skip_preview()?;
-    let (bricks, _) = reader.read_bricks(&header1, &header2)?;
+    let (mut bricks, _) = reader.read_bricks(&header1, &header2)?;
 
     let mut micro_owners = HashSet::new();
     let mut cleared_owners = HashSet::new();
 
-    for brick in bricks.into_iter() {
+    for brick in bricks.iter() {
         let asset = header2.brick_assets[brick.asset_name_index as usize].as_str();
         if asset.contains("Micro") {
             // this is a microbrick! figure out who owns it
@@ -110,10 +126,12 @@ async fn check_save(omegga: &Omegga, config: &Config, path: PathBuf) -> Result<(
                     let ts: u64 = s.parse()?;
                     let now = Utc::now().timestamp() as u64;
 
-                    if now >= ts + config.clear_after as u64 * 60 {
+                    if now >= ts + (config.clear_after * 60.) as u64 {
                         // clear bricks
-                        omegga.broadcast(format!("Clearing <color=\"ff0\">{}</>'s <color=\"ff0\">{}</> bricks (microbrick violation)...", owner.name, owner.bricks));
-                        omegga.clear_bricks(owner.id.to_string(), false);
+                        omegga.broadcast(format!(
+                            "Clearing <color=\"ff0\">{}</>'s microbricks...",
+                            owner.name
+                        ));
                         cleared_owners.insert(owner.id);
                     } else {
                         // warn the player
@@ -126,9 +144,11 @@ async fn check_save(omegga: &Omegga, config: &Config, path: PathBuf) -> Result<(
                     let ts = Utc::now().timestamp() as u64;
 
                     // if the clear_after amount is 0, just immediately clear bricks
-                    if config.clear_after == 0 {
-                        omegga.broadcast(format!("Clearing <color=\"ff0\">{}</>'s <color=\"ff0\">{}</> bricks (microbrick violation)...", owner.name, owner.bricks));
-                        omegga.clear_bricks(owner.id.to_string(), false);
+                    if config.clear_after == 0. {
+                        omegga.broadcast(format!(
+                            "Clearing <color=\"ff0\">{}</>'s microbricks...",
+                            owner.name
+                        ));
                         cleared_owners.insert(owner.id);
                     } else {
                         micro_owners.insert(owner.id);
@@ -139,6 +159,97 @@ async fn check_save(omegga: &Omegga, config: &Config, path: PathBuf) -> Result<(
             }
         }
     }
+
+    // clear violator bricks
+    for id in cleared_owners.iter() {
+        omegga.clear_bricks(id.to_string(), true);
+
+        let key = format!("violations:{}", id);
+        let mut violations: i64 = omegga
+            .store_get(key.clone())
+            .await?
+            .map(|v| v.as_i64().unwrap())
+            .unwrap_or(0);
+        violations += 1;
+
+        omegga.log(format!(
+            "Clearing bricks of {} ({} violations)",
+            id, violations
+        ));
+
+        omegga.store_set(key, violations.into());
+
+        if violations as u32 > config.max_violations {
+            // we've hit max violations: start banning the user
+            let key = format!("bans:{}", id);
+            let mut bans: i64 = omegga
+                .store_get(key.clone())
+                .await?
+                .map(|v| v.as_i64().unwrap())
+                .unwrap_or(0);
+            bans += 1;
+
+            omegga.store_set(key, bans.into());
+
+            if bans as u32 > config.max_bans {
+                // permanently ban
+                omegga.writeln(format!(
+                    "Chat.Command /Ban {} {} \"Microbricks are not allowed on this server.\"",
+                    id, "-1",
+                ));
+            } else {
+                // temporarily ban
+                omegga.writeln(format!(
+                    "Chat.Command /Ban {} {} \"Microbricks are not allowed on this server. This ban will be permanent in {} more violations.\"",
+                    id,
+                    config.ban_time * 60_000.,
+                    config.max_bans - bans as u32,
+                ));
+            }
+        } else {
+            omegga.whisper(
+                id.to_string(),
+                format!(
+                    "<b>You currently have {} microbrick violations. At {}, you will be temporarily banned.</>",
+                    violations,
+                    config.max_violations
+                )
+            );
+        }
+    }
+
+    // now, we should have a list of users whose bricks are cleared
+    // filter out bricks that were NOT placed by someone in this microbrick array
+    bricks.retain(|b| {
+        b.owner_index > 0
+            && cleared_owners.contains(&header2.brick_owners[b.owner_index as usize - 1].id)
+    });
+
+    // now keep only bricks without "Micro" in their asset name
+    bricks.retain(|b| !header2.brick_assets[b.asset_name_index as usize].contains("Micro"));
+
+    // now we've filtered out the bricks, so we can load everything back in as is
+    let save_data = SaveData {
+        header1,
+        header2,
+        bricks,
+        ..Default::default()
+    };
+
+    SaveWriter::new(
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(format!("{}/{}", SAVES_LOC, SAVE_LOC))?,
+        save_data,
+    )
+    .write()?;
+
+    // artificial delay: we are literally too fast for brickadia
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // load it into the game
+    omegga.load_bricks(SAVE_LOC, true, (0, 0, 0)).await?;
 
     // at this point check if there are users with a timestamp that were not found in this scan
     let keys = omegga.store_keys().await?;
@@ -155,5 +266,5 @@ async fn check_save(omegga: &Omegga, config: &Config, path: PathBuf) -> Result<(
 }
 
 fn warn_player(omegga: &Omegga, target: impl ToString) {
-    omegga.whisper(target.to_string(), "<size=\"30\"><color=\"a00\">Microbricks are not allowed on this server!</> Please delete your microbricks or <b>your bricks will be cleared</>.</>");
+    omegga.whisper(target.to_string(), "<size=\"30\"><color=\"a00\">Microbricks are not allowed on this server!</> Please delete your microbricks or <b>they will be cleared</>.</>");
 }
